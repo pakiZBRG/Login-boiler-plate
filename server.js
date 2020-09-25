299f7f593546737bad54910b4dfbafe87a865a5d
unknown <pavlovicnikola511@gmail.com>
2020-09-25 16:44:11 +0200

    production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 
@@ -14,6 +15,14 @@ if(process.env.NODE_ENV === 'development'){
     app.use(cors({ origin: process.env.CLIENT_URL }));
     app.use(morgan('dev'));
 }
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 //Routes
 app.use("/users", require('./routes/users'));
