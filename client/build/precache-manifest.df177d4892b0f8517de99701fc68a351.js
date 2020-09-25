c9f8357b2e1c7ca42af736c07570bcada83cf936
unknown <pavlovicnikola511@gmail.com>
2020-09-25 17:04:55 +0200

    more changes

---
+++ client/build/precache-manifest.df177d4892b0f8517de99701fc68a351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fbeee63710ee5fb800841fd948d4a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c902104e1b23564b90",
+    "url": "/static/css/main.13cf3721.chunk.css"
+  },
+  {
+    "revision": "f0435df6186b51335394",
+    "url": "/static/js/2.de051e38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de051e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c902104e1b23564b90",
+    "url": "/static/js/main.781ba119.chunk.js"
+  },
+  {
+    "revision": "0dc9215c4b0a17c8c874",
+    "url": "/static/js/runtime-main.7bf3d854.js"
+  },
+  {
+    "revision": "624ce12d69bc3508324cef9084a6434c",
+    "url": "/static/media/register.624ce12d.jpg"
+  }
+]);
