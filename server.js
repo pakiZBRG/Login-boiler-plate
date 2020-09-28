de26f9e65127bd3db1479f0d2e8bb29ad4c71cac
unknown <pavlovicnikola511@gmail.com>
2020-09-28 10:05:07 +0200

    changes

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    // app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 }
 
