3ced1357ef1d64e87e9dce5ee06d7df2458e597a
unknown <pavlovicnikola511@gmail.com>
2020-11-14 10:03:55 +0100

    dir change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV === 'development'){
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('public'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
     });
 }
 
