7e9757aba339b4fed1d4d434d554f2265c22827e
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-07-06 10:21:33 +0200

    Production ready

--- server.js
+++ server.js
@@ -39,18 +39,16 @@ mongoose.connect(process.env.MONGO_URI, {
     .then(() => console.log("MongoDB connected..."))
     .catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
-
 //Routes
 app.use("/users", require('./routes/users'));
 app.get('/', (req, res) => {
-    res.send("Server running")
-})
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+// Production Ready
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
